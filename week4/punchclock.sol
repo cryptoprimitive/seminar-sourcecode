c4b8ae74161cdf5d82f110d50f1e2dda6e4231dc
oglog <coinop.logan@gmail.com>
2018-07-26 20:03:59 +0000

    forgot to update contract code!

--- week4/punchclock.sol
+++ week4/punchclock.sol
@@ -1,10 +1,10 @@
 pragma solidity ^0.4.24;
 
 contract Punchclock {
-    event PunchEvent(address agent, string action);
+    event PunchEvent(address agent, string action, uint time);
 
     function punch(string _action)
     external {
-        emit PunchEvent(msg.sender, _action);
+        emit PunchEvent(msg.sender, _action, now);
     }
 }
