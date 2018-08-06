b97c08ebdd12765783edea04f402f2e659263d35
oglog <coinop.logan@gmail.com>
2018-08-06 19:22:11 +0000

    moved fee capture to Factory contract

--- week6/SellOffer.sol
+++ week6/SellOffer.sol
@@ -3,6 +3,9 @@ pragma solidity ^ 0.4.24;
 import "browser/BurnablePayment.sol";
 
 contract ToastytradeOfferFactory {
+    address public notifyServiceProvider = 0x0;
+    uint public notifyServiceFee = 1;
+
     address[] public toastytradeOffers;
 
     event OfferCreated(address OfferAddress, address BurnablePaymentAddress, string offerDetails);
@@ -10,39 +13,36 @@ contract ToastytradeOfferFactory {
     function newToastytradeOffer(address seller, uint autoreleaseInterval, string _offerDetails)
     payable
     external {
-        ToastytradeOffer offer = (new ToastytradeOffer).value(msg.value)(seller, autoreleaseInterval, _offerDetails);
+        require(msg.value >= notifyServiceFee, "Not enough ether to cover the notify service fee");
+        uint valueToForwardToOffer = msg.value - notifyServiceFee;
+
+        ToastytradeOffer offer = (new ToastytradeOffer).value(valueToForwardToOffer)(seller, autoreleaseInterval, _offerDetails);
 
         emit OfferCreated(address(offer), offer.offerBP(), _offerDetails);
 
         toastytradeOffers.push(address(offer));
+
+        //send the fee to our server
+        notifyServiceProvider.transfer(notifyServiceFee);
+
+        //Make sure there's no leftover ether
+        assert(address(this).balance == 0);
     }
 
 }
 
 contract ToastytradeOffer {
-    address public notifyServiceProvider = 0x0;
-    uint public notifyServiceFee = 1;
-
     address public offerBP;
 
     string public offerDetails;
 
     constructor(address seller, uint autoreleaseInterval, string _offerDetails)
     payable {
-        require(msg.value >= notifyServiceFee, "Not enough ether to cover the notify service fee");
-        uint valueToForwardToBP = msg.value - notifyServiceFee;
-
         //Prepare vars and create BP
         uint commitThreshold = msg.value / 3;
 
-        offerBP = (new BurnablePayment).value(valueToForwardToBP)(true, seller, commitThreshold, autoreleaseInterval, "", "");
+        offerBP = (new BurnablePayment).value(msg.value)(true, seller, commitThreshold, autoreleaseInterval, "", "");
 
         offerDetails = _offerDetails;
-
-        //send a fee to our server
-        notifyServiceProvider.transfer(notifyServiceFee);
-
-        //Make sure there's no leftover ether
-        assert(address(this).balance == 0);
     }
 }
